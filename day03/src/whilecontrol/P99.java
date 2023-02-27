7f73f950307a47f02037ba2c3664765b34e8be8f
shinzkdls <shinzkdls@gmail.com>
2023-02-27 09:39:49 +0900

    add d

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -6,6 +6,7 @@ public class P99 {
 		int bb = 1000;
 		int aa = 1000;
 		int cc = 100;
+		int d = 20;
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
