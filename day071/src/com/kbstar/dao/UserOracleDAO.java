751e3ea7cdaee7472ecc4f6d3ede15376098fa5c
shinzkdls <shinzkdls@gmail.com>
2023-03-07 09:06:57 +0900

    0306 interface commit

---
+++ day071/src/com/kbstar/dao/UserOracleDAO.java
@@ -0,0 +1,25 @@
+package com.kbstar.dao;
+
+import com.kbstar.frame.DAO;
+
+public class UserOracleDAO implements DAO {
+
+	@Override
+	public void insert(Object obj) {
+		System.out.println(obj);
+		System.out.println("Inserted MariaDB..."); // 실제 구현단계에서 MariaDB로 변경됐을때 바꿀건 DAO뿐
+	}
+
+	@Override
+	public void delete(Object obj) {
+		System.out.println(obj);
+		System.out.println("Deleted MariaDB..."); // 실제 구현단계에서 MariaDB로 변경됐을때 바꿀건 DAO뿐
+	}
+
+	@Override
+	public void update(Object obj) {
+		System.out.println(obj);
+		System.out.println("Updated MariaDB..."); // 실제 구현단계에서 MariaDB로 변경됐을때 바꿀건 DAO뿐
+	}
+
+}
